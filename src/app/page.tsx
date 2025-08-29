e9ff1f58d4d8aa403fbd05975ddec5c1774a4254
Sereymony Ros <sereymonyros@gmail.com>
2025-08-29 09:51:21 +0000

    in mobile make more space between result and main title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { KhmerEzCard } from "@/components/khmer-ez-card";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen w-full flex-col items-center justify-center bg-background p-4 font-body pt-24">
+    <main className="flex min-h-screen w-full flex-col items-center justify-center bg-background p-4 font-body pt-32">
       <div className="flex flex-col items-center justify-center space-y-2 text-center">
         <h1 className="text-4xl font-bold tracking-tight text-foreground sm:text-5xl font-headline">
           KhmerEZ
